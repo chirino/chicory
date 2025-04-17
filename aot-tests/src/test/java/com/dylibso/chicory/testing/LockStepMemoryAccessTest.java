8127a0d32356802563e759e36b8582635cf5686b
Hiram Chirino <hiram@hiramchirino.com>
2025-04-16 20:27:29 -0400

    Test wat2wasm aot/interpreter has the same lockstep execution.
    Signed-off-by: Hiram Chirino <hiram@hiramchirino.com>
    (cherry picked from commit c13f054aa3429a95c66e7f7f375e076e4d3deed4)

--- aot-tests/src/test/java/com/dylibso/chicory/testing/LockStepMemoryAccessTest.java
+++ aot-tests/src/test/java/com/dylibso/chicory/testing/LockStepMemoryAccessTest.java
@@ -2,10 +2,12 @@ package com.dylibso.chicory.testing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.dylibso.chicory.corpus.WatGenerator;
 import com.dylibso.chicory.experimental.aot.AotMachine;
 import com.dylibso.chicory.runtime.ByteArrayMemory;
 import com.dylibso.chicory.runtime.Instance;
 import com.dylibso.chicory.runtime.Memory;
+import com.dylibso.chicory.wabt.Wat2Wasm;
 import com.dylibso.chicory.wasm.Parser;
 import com.dylibso.chicory.wasm.types.MemoryLimits;
 import java.io.IOException;
@@ -23,9 +25,9 @@ public class LockStepMemoryAccessTest {
         var pair =
                 LockStepMemory.create(
                         name1,
-                        new ByteArrayMemory(new MemoryLimits(1)),
+                        new ByteArrayMemory(new MemoryLimits(1024, 65536)),
                         name2,
-                        new ByteArrayMemory(new MemoryLimits(1)));
+                        new ByteArrayMemory(new MemoryLimits(1024, 65536)));
         new Thread(
                         () -> {
                             workload.run((x) -> pair[1], name2);
@@ -79,8 +81,25 @@ public class LockStepMemoryAccessTest {
     }
 
     @Test
-    public void AotAndInterpretersHaveSameMemoryAccess() throws IOException {
+    public void aotAndInterpretersHaveSameMemoryAccess() throws IOException {
         var pair = assertHasSameMemoryAccess("interpreter", "aot", this::testMemoryWat);
+        assertEquals(14, pair[0].eventCounter());
+    }
+
+    private void testWat2Wasm(Function<MemoryLimits, Memory> memoryFactory, String name) {
+        var options = Wat2Wasm.options().withMemoryFactory(memoryFactory);
+        if (name.startsWith("aot")) {
+            options.withExecutionType(Wat2Wasm.ExecutionType.AOT);
+        } else {
+            options.withExecutionType(Wat2Wasm.ExecutionType.INTERPRETED);
+        }
+        var wat = WatGenerator.bigWat(10_000, 0);
+        Wat2Wasm.parse(wat, options);
+    }
+
+    @Test
+    public void wat2wasmAotAndInterpretersHaveSameMemoryAccess() throws IOException {
+        var pair = assertHasSameMemoryAccess("aot", "interpreter", this::testWat2Wasm);
         assertEquals(14, pair[0].eventCounter());
     }
 }
